631299cf9ca3786e27ec209bd0b385f06f050171
Oleksandr Sydorskyi <88611124+oleksandr-sydorskyi@users.noreply.github.com>
2024-04-21 11:06:35 +0300

    REQ-04: Add a new pet

---
+++ src/main/java/com/magicvet/service/PetService.java
@@ -0,0 +1,50 @@
+package main.java.com.magicvet.service;
+
+import main.java.com.magicvet.Main;
+import main.java.com.magicvet.model.Dog;
+import main.java.com.magicvet.model.Pet;
+
+public class PetService {
+
+    private static String DOG_TYPE = "dog";
+
+    public Pet registerNewPet() {
+        Pet pet;
+        System.out.print("Type (dog / cat / other: ");
+        String type = Main.SCANNER.nextLine();
+        if (DOG_TYPE.equals(type)) {
+            pet = buildDog();
+        } else {
+            pet = buildPet(type);
+        }
+        return pet;
+    }
+
+    private Dog buildDog() {
+        Pet pet = buildPet(DOG_TYPE);
+        Dog dog = petToDog(pet);
+        System.out.print("Size (XS / S / M / L / XXL): ");
+        dog.setSize(Main.SCANNER.nextLine());
+        return dog;
+    }
+
+    private Dog petToDog(Pet pet) {
+        Dog dog = new Dog();
+        dog.setAge(pet.getAge());
+        dog.setName(pet.getName());
+        dog.setSex(pet.getSex());
+        return dog;
+    }
+
+    private Pet buildPet(String type) {
+        Pet pet = new Pet();
+        pet.setType(type);
+        System.out.print("Age: ");
+        pet.setAge(Main.SCANNER.nextLine());
+        System.out.print("Name: ");
+        pet.setName(Main.SCANNER.nextLine());
+        System.out.print("Sex (male / female): ");
+        pet.setSex(Main.SCANNER.nextLine());
+        return pet;
+    }
+}
